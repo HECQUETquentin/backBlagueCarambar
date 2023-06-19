7cc8fd258d8c8472f9dd154f20cd6c7b42900b3a
Quentin Hecquet <quentin.hecquet@outlook.fr>
2023-06-19 17:07:51 +0200

    update swagger

--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,37 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/controller')
 
+/**
+ * @swagger
+ * /api/v1/findAllJokes:
+ *  get:
+ *    description: affichage de la totalité des blagues de la base
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.get("/findAllJokes", controller.findAllJokes)
 
+/**
+ * @swagger
+ * /api/v1/findAllJokes:
+ *  get:
+ *    description: affichage d'une blague en fonction de son id
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.get("/findJokeById/:id", controller.findJokeById)
 
+/**
+ * @swagger
+ * /api/v1/getRandomJoke:
+ *  get:
+ *    description: affichage d'une blague dans celle inscrite dans la base
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.get("/getRandomJoke", controller.getRandomJoke)
 
 module.exports = router;
